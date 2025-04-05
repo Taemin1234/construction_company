3a72d8c54fc86937195f5d12426023b23cc338ef
King sejong <cestmoi7531@gmail.com>
2025-04-05 21:07:16 +0900

    hero 페이지지

---
+++ frontend/src/Pages/MainPage/Forum.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Forum = () => {
+    return (
+        <div>
+            forun   m
+        </div>
+    )
+}
+
+export default Forum
---
+++ frontend/src/Pages/MainPage/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero'
+import Forum from './Forum'
+import Contacts from './Contacts'
+
+const MainPage = () => {
+    return (
+        <div>
+            <Hero />
+            <Forum />
+            <Contacts />
+        </div>
+    )
+}
+
+export default MainPage
