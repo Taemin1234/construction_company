de8d512b6292e70875da5f29cb26c71a150c996f
King sejong <cestmoi7531@gmail.com>
2025-04-10 20:05:42 +0900

    관리자 페이지 접근 제어

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -53,6 +53,41 @@ function AuthRedirectRoute() {
   return isAuthenticated ? <Navigate to="/admin/posts" replace /> : <Outlet />;
 }
 
+// 관리자 페이지 접근 제어(토큰이 있는 경우만 접근 가능)
+function ProtectedRoute() {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const [user, setUser] = useState(null); // 유저에 대한 정보
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/api/auth/verify-token",
+          {},
+          { withCredentials: true }
+        );
+        setIsAuthenticated(response.data.isValid); //res의 반환값
+        setUser(response.data.user);
+      } catch (error) {
+        console.log("토큰 인증 실패: ", error);
+        setIsAuthenticated(false);
+        setUser(null);
+      }
+    };
+    verifyToken();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  return isAuthenticated ? (
+    <Outlet context={{ user }} /> // 접속이 가능하면 context로 받아온 user의 정보를 전달
+  ) : (
+    <Navigate to="/admin" replace />
+  )
+}
+
 function Layout() {
   return (
     <>
@@ -110,25 +145,30 @@ const router = createBrowserRouter([
   },
   {
     path: "/admin",
-    element: <AdminLayout />,
+    element: <ProtectedRoute />, // 토큰을 검증하는 함수를 호출해서 검증
     children: [
       {
-        path: "posts",
-        element: <AdminPosts />,
-      },
-      {
-        path: "create-post",
-        element: <AdminCreatePost />,
-      },
-      {
-        path: "edit-post/:id",
-        element: <AdminEditPost />,
-      },
-      {
-        path: "contacts",
-        element: <AdminContacts />,
-      },
-    ],
+        element: <AdminLayout />,
+        children: [
+          {
+            path: "posts",
+            element: <AdminPosts />,
+          },
+          {
+            path: "create-post",
+            element: <AdminCreatePost />,
+          },
+          {
+            path: "edit-post/:id",
+            element: <AdminEditPost />,
+          },
+          {
+            path: "contacts",
+            element: <AdminContacts />,
+          },
+        ],
+      }
+    ]
   },
 ]);
 
